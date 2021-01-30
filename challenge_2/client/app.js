ffbc3f9ad4848c6a5c7ee3dc15e844fe2bf1c4b7
James Henderson <jhende1995@gmail.com>
2021-01-30 00:49:48 -0500

    complete textarea convert

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,27 @@
+//SETTINGS
+var settings = {
+  server: {
+    url: 'http://localhost:42069'
+  },
+  xhr: new XMLHttpRequest()
+};
 
+//INIT
+function onload() {
+  document.querySelector('#inputForm').addEventListener('submit', e => {
+    e.preventDefault();
+    requestCSV();
+  });
 
+  settings.xhr.onreadystatechange = (e) => {
+    if(settings.xhr.readyState === 4) {
+      document.querySelector('#results').innerHTML = settings.xhr.responseText;
+    }
+  }
+}
+
+function requestCSV() {
+  settings.xhr.open("POST", `${settings.server.url}/convert`, true);
+  settings.xhr.setRequestHeader('Content-type', 'application/json');
+  settings.xhr.send(JSON.stringify({data: document.querySelector('#JSON').value}));
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,85 @@
+//define server
+var express = require('express');
+var cors = require('cors');
+var app = express();
+var port = 42069;
+
+var corsOptions = {
+  "origin": '*',
+  "allowedHeaders": 'Content-Type,Content-type',
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 
+//define routes
+app.post('/convert', (req, res) => {
+
+  try {
+    var data = req.body.data;
+    if (data[data.length - 1] === ';') {
+      data = data.slice(0, -1);
+    }
+    var parsedData = JSON.parse(data);
+
+    res.status(200).send(toCSV(parsedData));
+
+  } catch (error) {
+
+    res.status(400).send('ERROR: INVALID JSON SYNTAX');
+
+  }
+});
+
+//init server
+app.listen(port, () => {
+  console.log(`CSV Generator listening on at http://localhost:${port}`);
+})
+
+
+//CONVERSION LOGIC
+function toCSV(jsonObj) {
+  var keys = [];
+  var values = [];
+
+  dfEach(jsonObj, (currentObj) => {
+    for (var key in currentObj) {
+      if(key !== 'children') {
+        if (keys.indexOf(key) === -1) {
+          keys.push(key);
+        }
+      }
+    }
+  });
+  dfEach(jsonObj, (currentObj) => {
+    values.push(valuesToStr(keys, currentObj));
+  });
+
+  var output = keys.join(',');
+  for(var i = 0; i < values.length; i++) {
+    output += '\n' + values[i];
+  }
+  console.log('OUTPUT OF CSV CONVERT: ');
+  console.log(output);
+  return output;
+
+}
+
+function valuesToStr(keyArr, jsonObj) {
+  var str = '';
+  for (var i = 0; i < keyArr.length; i++) {
+    if( i !== 0) {
+      str += ',';
+    }
+    str+=jsonObj[keyArr[i]];
+  }
+  return str;
+}
+
+function dfEach(obj, cb) {
+  cb(obj);
+  obj.children.forEach((child) => {
+    dfEach(child, cb);
+  })
+}
