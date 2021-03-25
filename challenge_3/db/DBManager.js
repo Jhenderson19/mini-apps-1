3c07af9ad0928f7a935cdc51b40d4db773040e13
James Henderson <jhende1995@gmail.com>
2021-03-25 14:10:40 -0400

    wrote user, address, payment insert statements

--- challenge_3/db/DBManager.js
+++ challenge_3/db/DBManager.js
@@ -1,8 +1,33 @@
 const Promise = require('bluebird');
-const connection = Promise.promisifyAll(require('./connection'));
+const db = Promise.promisifyAll(require('./connection'));
 
-class DBManager {
+class DataManager {
   constructor() {}
 
-
+  insertUser(user) {
+    return db.queryAsync(`INSERT INTO 'users' ('name','email','password')
+    VALUES ('${user.name}','${user.email}','${user.password}');`)
+    .then((queryResults) => {
+      console.log(queryResults);
+      return queryResults;
+    })
+  }
+  insertAddress(address) {
+    return db.queryAsync(`INSERT INTO 'addresses' ('line1','line2','city','state','zipCode','phoneNum')
+    VALUES ('${address.line1}','${address.line2 !== undefined ? address.line2 : 'null'}','${address.city}','${address.state}','${address.zipCode}','${address.phoneNum}');`)
+    .then((queryResults) => {
+      console.log(queryResults);
+      return queryResults;
+    })
+  }
+  insertPayment(payment) {
+    return db.queryAsync(`INSERT INTO 'payment' ('cardNum','exp','sercurityCode','zipCode')
+    VALUES ('${payment.cardNum}','${payment.exp}','${payment.securityCode}','${payment.zipCode}');`)
+    .then((queryResults) => {
+      console.log(queryResults);
+      return queryResults;
+    })
+  }
 }
+
+module.exports = new DataManager();
